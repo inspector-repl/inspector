8c452af1295c84ff908eedae922dc99a25713477
Jörg Thalheim <joerg@thalheim.io>
2017-07-25 23:32:43 +0100

    inspector::cflags print rpath

--- python/inspector/cflags.py
+++ python/inspector/cflags.py
@@ -7,4 +7,5 @@ def print_cflags(args):
     data = dict(macro=INSPECTOR_MACRO,
                 library_path=LIBRARY_PATH,
                 include_path=INCLUDE_PATH)
-    print("-I {include_path} -I .inspector-includes \'{macro}\' -L{library_path} -linspector".format(**data))
+
+    print("-I {include_path} -I .inspector-includes \'{macro}\' -Wl,-rpath,{library_path} -L{library_path} -linspector".format(**data))
