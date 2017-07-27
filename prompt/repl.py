df507440bf4963cc3e9ab31b8a06ec685cb3aeef
Jörg Thalheim <joerg@thalheim.io>
2017-07-27 02:26:00 +0100

    add clang completion

--- prompt/repl.py
+++ prompt/repl.py
@@ -4,8 +4,37 @@ import json
 from prompt_toolkit import prompt as prompt_tk
 from prompt_toolkit.history import InMemoryHistory
 import pygments
+from subprocess import Popen, PIPE
 from pygments.lexers import CppLexer
 from pygments.formatters import TerminalFormatter
+from prompt_toolkit.completion import Completer, Completion
+
+class ClangCompleter(Completer):
+    def __init__(self, file_path, line):
+        self.file_path = file_path
+        self.line = line
+        with open(self.file_path) as f:
+            self.content = f.readlines()
+    def run_clang(self, document):
+        column = len(document) + 1
+        cmd = "clang++ -fsyntax-only -Xclang -code-completion-macros -Xclang -code-completion-at=-:%d:%d -x c++ -" % (self.line, column)
+        self.content[self.line - 1] = document
+        p = Popen(cmd.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        print("\n".join(self.content))
+        print(cmd)
+        stdout, stderr = p.communicate("".join(self.content).encode("utf-8"))
+        lines = stdout.split(b"\n")
+        symbols = []
+        for line in lines:
+            parts = line.split(b": ")
+            if len(parts) != 3 or parts[0] != b"COMPLETION":
+                continue
+            symbols.append(parts[1].rstrip().decode("utf-8"))
+        return symbols
+    def get_completions(self, document, complete_event):
+        completions = self.run_clang(document.current_line_before_cursor)
+        for completion in completions:
+            yield Completion(completion, start_position=0)
 
 
 class Repl():
@@ -57,9 +86,15 @@ class Repl():
 
     def run_interpreter_loop(self):
         history = InMemoryHistory()
+        try:
+            completer = ClangCompleter(self.file_path, self.line_number)
+        except IOError:
+            completer = None
         while True:
             prompt = self._prompt_string()
-            answer = prompt_tk(prompt, history=history, lexer=CppLexer)
+            answer = prompt_tk(prompt, history=history, lexer=CppLexer, completer=completer)
+            if answer == '.quit':
+                break
             response = json.dumps(dict(input=answer), ensure_ascii=True)
             self.output.sendall(response.encode("utf-8"))
             self.output.sendall(b'\0')
