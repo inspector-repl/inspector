cde715d6278793b5d558edc0cdfb28bdb4605fca
Jörg Thalheim <joerg@thalheim.io>
2017-07-27 13:48:59 +0100

    remove cpprest code again

--- lib/inspector.cpp
+++ lib/inspector.cpp
@@ -17,93 +17,58 @@
 
 #define RCVBUFSIZE 10240
 
+std::string getJsonInspectorLocation(std::string filePath, unsigned lineNumber){
+  Json::Value message;
+
+  message["file"] = filePath.c_str();
+  message["line"] = lineNumber;
+
+  Json::StreamWriterBuilder wbuilder;
+  return Json::writeString(wbuilder, message);
+}
+
+std::string getJsonValue(std::string value){
+  Json::Value message;
+
+  message["value"] = value.c_str();
+
+  Json::StreamWriterBuilder wbuilder;
+  return Json::writeString(wbuilder, message);
+}
+
+void sendInspectorLocationToPrompt(std::string path, unsigned lineNumber, TCPSocket& prompt){
+  std::string location = getJsonInspectorLocation(path, lineNumber);
+  prompt.send(location.c_str(), location.length() + 1);
+}
+
+void sendResultToPrompt(std::string value, TCPSocket& prompt){
+  std::string v = getJsonValue(value);
+  prompt.send(v.c_str(), v.length() + 1);
+}
+
+std::string getCppStatementFromPrompt(TCPSocket& prompt){
+  char receiveBuffer[RCVBUFSIZE];
+  int bytesReceived = 0;
+  int totalBytesReceived = 0;
+
+  do {
+    if ((bytesReceived = (prompt.recv(receiveBuffer + totalBytesReceived, RCVBUFSIZE - totalBytesReceived))) <= 0) {
+      cerr << "Unable to read";
+      exit(1);
+    }
+    totalBytesReceived += bytesReceived;
+  } while (totalBytesReceived < RCVBUFSIZE && receiveBuffer[totalBytesReceived - 1] != '\0');
+
+  receiveBuffer[totalBytesReceived - 1] = '\0';
+
+  std::stringstream ss(receiveBuffer);
+  Json::Value jsonMessage;
+  ss >> jsonMessage;
+  std::string inputValue = jsonMessage["input"].asString();
+  return inputValue;
+}
+
 extern "C" {
-  void printJsonMessage(Json::Value message){
-    std::cout << message << std::endl;
-  }
-
-  std::string sendRequest(std::string message){
-    // Http client.
-    web::http::client::http_client client(U("http://localhost:5000"));
-
-    // URI builder.
-    web::uri_builder builder(U("/"));
-
-    web::json::value body = web::json::value::parse(message);
-    pplx::task<web::http::http_response> resp = client.request(web::http::methods::POST, builder.to_string(), body);
-    web::json::value response = resp.get().extract_json().get();
-    std::string result = response["result"].as_string();
-    return result;
-  }
-
-  std::string getJsonRequest(
-        unsigned id,
-        std::string method,
-        std::string path,
-        unsigned lineNumber,
-        std::string clingContext) {
-    Json::Value request;
-    request["id"] = id;
-    request["jsonrpc"] = "2.0";
-    request["method"] = method.c_str();
-    request["params"]["path"] = path.c_str();
-    request["params"]["lineNumber"] = lineNumber;
-    request["params"]["clingContext"] = clingContext.c_str();
-
-    Json::StreamWriterBuilder wbuilder;
-    return Json::writeString(wbuilder, request);
-  }
-
-  std::string getJsonInspectorLocation(std::string filePath, unsigned lineNumber){
-    Json::Value message;
-
-    message["file"] = filePath.c_str();
-    message["line"] = lineNumber;
-
-    Json::StreamWriterBuilder wbuilder;
-    return Json::writeString(wbuilder, message);
-  }
-
-  std::string getJsonValue(std::string value){
-    Json::Value message;
-
-    message["value"] = value.c_str();
-
-    Json::StreamWriterBuilder wbuilder;
-    return Json::writeString(wbuilder, message);
-  }
-
-  void sendInspectorLocationToPrompt(std::string path, unsigned lineNumber, TCPSocket& prompt){
-    std::string location = getJsonInspectorLocation(path, lineNumber);
-    prompt.send(location.c_str(), location.length() + 1);
-  }
-
-  void sendResultToPrompt(std::string value, TCPSocket& prompt){
-    std::string v = getJsonValue(value);
-    prompt.send(v.c_str(), v.length() + 1);
-  }
-
-  std::string getCppStatementFromPrompt(TCPSocket& prompt){
-    char receiveBuffer[RCVBUFSIZE];
-    int bytesReceived = 0;
-    int totalBytesReceived = 0;
-
-    do {
-      if ((bytesReceived = (prompt.recv(receiveBuffer + totalBytesReceived, RCVBUFSIZE - totalBytesReceived))) <= 0) {
-        cerr << "Unable to read";
-        exit(1);
-      }
-      totalBytesReceived += bytesReceived;
-    } while (totalBytesReceived < RCVBUFSIZE && receiveBuffer[totalBytesReceived - 1] != '\0');
-
-    receiveBuffer[totalBytesReceived - 1] = '\0';
-
-    std::stringstream ss(receiveBuffer);
-    Json::Value jsonMessage;
-    ss >> jsonMessage;
-    std::string inputValue = jsonMessage["input"].asString();
-    return inputValue;
-  }
 
   void inspectorRunRepl(const char* path, unsigned lineNumber, const char* clingDeclare, const char* clingContext, ...) {
     va_list arglist;
