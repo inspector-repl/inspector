3a9b8e6b3e5c39df00d65c53cc46e4b391febf8a
Jörg Thalheim <joerg@thalheim.io>
2025-08-23 13:41:00 +0200

    drop unused includes

--- include/inspector/repl.h
+++ include/inspector/repl.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#include "inspector/common.h"
-
-#include "string"
-
 extern "C" {
 void inspectorRunRepl(const char *path, unsigned lineNumber,
                       const char *clingDeclare, const char *clingContext,
